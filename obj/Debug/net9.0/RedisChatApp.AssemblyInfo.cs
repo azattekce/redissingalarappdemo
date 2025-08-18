f95cc799387bb0727ccd7e98106e7feff0c0b4ba
Azat Tekçe <Azat.Tekce@opet.com.tr>
2025-08-18 15:45:23 +0300

    Geliştirildi: AssemblyInfo dosyasında versiyon bilgisi güncellendi; index.html dosyasında toast bildirimleri merkezi konuma alındı; yeni çözüm dosyası oluşturuldu.

--- obj/Debug/net9.0/RedisChatApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/RedisChatApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedisChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd793b407824b724f8a6934cffc06246fc8d99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedisChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedisChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
